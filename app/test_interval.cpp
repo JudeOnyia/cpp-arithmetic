0fd81b4d2225985328adcc597aaf191bb0cb2421
Jude Onyia <judeonyia10@gmail.com>
2020-06-11 18:12:27 -0700

    1) wrote indeterminate_result class 2) wrote rounding_mode_saver class 3) wrote statistics class 4) wrote default constructor and compound operator overloads (+,-,*) 5) wrote two argument constructor 6) wrote upper, lower, is_singleton and sign member functions

--- app/test_interval.cpp
+++ app/test_interval.cpp
@@ -1,5 +1,40 @@
+#include"ra/interval.hpp"
+#include<iostream>
 
 int main(){
+	using itvf = typename ra::math::interval<float>;
+	using std::cout;
+	using std::endl;
+
+	// Test default constructor
+	itvf obj_A;
+	cout<<"obj_A: "<<(obj_A.lower())<<" "<<(obj_A.upper())<<endl;
+	// Test 2 value constructor
+	itvf obj_B(2.89, 2.91);
+	itvf obj_C(3.91,3.89);
+	cout<<"obj_B: "<<(obj_B.lower())<<" "<<(obj_B.upper())<<endl;
+	cout<<"obj_C: "<<(obj_C.lower())<<" "<<(obj_C.upper())<<endl;
+	// Test Compound add, sub, mult
+	cout<<"obj_C+=obj_B: "<<((obj_C+=obj_B).lower())<<" "<<(obj_C.upper())<<endl;
+	cout<<"obj_C-=obj_B: "<<((obj_C-=obj_B).lower())<<" "<<(obj_C.upper())<<endl;
+	cout<<"obj_C*=obj_B: "<<((obj_C*=obj_B).lower())<<" "<<(obj_C.upper())<<endl;
+	// Test Aliasing of Compound add, sub, mult
+	cout<<"obj_B+=obj_B: "<<((obj_B+=obj_B).lower())<<" "<<(obj_B.upper())<<endl;
+	cout<<"obj_B-=obj_B: "<<((obj_B-=obj_B).lower())<<" "<<(obj_B.upper())<<endl;
+	cout<<"obj_B*=obj_B: "<<((obj_B*=obj_B).lower())<<" "<<(obj_B.upper())<<endl;
+	// Test is_singleton
+	itvf obj_D(1.7);
+	cout<<"obj_D.is_singleton() must be true: "<<(obj_D.is_singleton())<<endl;
+	cout<<"obj_C.is_singleton() must be false: "<<(obj_C.is_singleton())<<endl;
+	// Test sign()
+	itvf obj_E(-7.335,-7.339);
+	itvf obj_F(3.442,3.448);
+	itvf obj_G(-0.04,0.12);
+	cout<<"Neg sign: "<<(obj_E.sign())<<endl;
+	cout<<"Pos sign: "<<(obj_F.sign())<<endl;
+	cout<<"Zero no sign: "<<(obj_A.sign())<<endl;
+	//cout<<"Indeteminate for sign: "<<(obj_G.sign())<<endl;
 	
+
 	return 0;
 }
--- include/ra/interval.hpp
+++ include/ra/interval.hpp
@@ -1,8 +1,108 @@
 #ifndef INTERVAL_HPP
 #define INTERVAL_HPP
-
+#include<cfenv>
+#include<stdexcept>
+#include<algorithm>
 namespace ra::math {
+	#pragma STDC FENV_ACCESS ON
+	struct indeterminate_result : public std::runtime_error{
+		using std::runtime_error::runtime_error;
+	};
 
+	class rounding_mode_saver {
+		public:
+			rounding_mode_saver() : current_round_mode(std::fegetround()) {}
+			~rounding_mode_saver() {
+				std::fesetround(current_round_mode);
+			}
+			rounding_mode_saver(rounding_mode_saver&&) = delete;
+			rounding_mode_saver(const rounding_mode_saver&) = delete;
+			rounding_mode_saver& operator=(rounding_mode_saver&&) = delete;
+			rounding_mode_saver& operator=(const rounding_mode_saver&) = delete;
+		private:
+			int current_round_mode;
+	};
+
+	template<class T>
+	class interval{
+		public:
+			using real_type = T;
+
+			struct statistics{
+				// The total number of indeterminate results encountered.
+				unsigned long indeterminate_result_count = 0;
+				// The total number of interval arithmetic operations.
+				unsigned long arithmetic_op_count = 0;
+			};
+
+			interval(real_type set_value = real_type(0)) : lower_(set_value), upper_(set_value) {}
+			interval(real_type lower, real_type upper) : lower_(lower), upper_(upper) {
+				if(lower > upper){ lower_=upper; upper_=lower; }
+			}
+			real_type lower() const { return lower_; }
+			real_type upper() const { return upper_; }
+			interval& operator+=(const interval& obj){
+				real_type upper_Temp(0);
+				real_type lower_Temp(0);
+				++(stat_.arithmetic_op_count);
+				rounding_mode_saver rms; // Save the rounding mode that should be restored
+				std::fesetround(FE_DOWNWARD);
+				lower_Temp = lower_ + obj.lower();
+				std::fesetround(FE_UPWARD);
+				upper_Temp = upper_ + obj.upper();
+				lower_ = lower_Temp;
+				upper_ = upper_Temp;
+				return *this;
+			}
+			interval& operator-=(const interval& obj){
+				real_type upper_Temp(0);
+				real_type lower_Temp(0);
+				++(stat_.arithmetic_op_count);
+				rounding_mode_saver rms; // Save the rounding mode that should be restored
+				std::fesetround(FE_DOWNWARD);
+				lower_Temp = lower_ - obj.upper();
+				std::fesetround(FE_UPWARD);
+				upper_Temp = upper_ - obj.lower();
+				lower_ = lower_Temp;
+				upper_ = upper_Temp;
+				return *this;
+			}
+			interval& operator*=(const interval& obj){
+				real_type upper_Temp(0);
+				real_type lower_Temp(0);
+				++(stat_.arithmetic_op_count);
+				rounding_mode_saver rms; // Save the rounding mode that should be restored
+				std::fesetround(FE_DOWNWARD);
+				lower_Temp = std::min(std::min((lower_*obj.lower()),(lower_*obj.upper())), std::min((upper_*obj.lower()), (upper_*obj.upper())));
+				std::fesetround(FE_UPWARD);
+				upper_Temp = std::max(std::max((lower_*obj.lower()),(lower_*obj.upper())), std::max((upper_*obj.lower()), (upper_*obj.upper())));
+				lower_ = lower_Temp;
+				upper_ = upper_Temp;
+				return *this;
+			}
+			
+			bool is_singleton() const { return (lower_==upper_)? true : false; }
+
+			int sign() const {
+				if((lower_ < real_type(0)) && (upper_ < real_type(0))){
+					return -1;
+				}
+				else if((lower_ > real_type(0)) && (upper_ > real_type(0))){
+					return 1;
+				}
+				else if((lower_ == real_type(0)) && (upper_ == real_type(0))){
+					return 0;
+				}
+				else{
+					throw indeterminate_result("Indeterminate result");
+				}
+			}
+		private:
+			real_type upper_;
+			real_type lower_;
+			statistics stat_;
+			
+	};
 
 
 }
