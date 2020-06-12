4d3078e339c39e99c68a742c23c559fa9c688e26
Jude Onyia <judeonyia10@gmail.com>
2020-06-12 02:43:44 -0700

    1) Wrote clear_statistics member function 2) Wrote get_statistics member function 3) Wrote binary add, sub, mult non-member functions 4) Wrote less than operator overload 5) Wrote stream inserter

--- app/test_interval.cpp
+++ app/test_interval.cpp
@@ -3,6 +3,7 @@
 
 int main(){
 	using itvf = typename ra::math::interval<float>;
+	using stat = typename ra::math::interval<float>::statistics;
 	using std::cout;
 	using std::endl;
 
@@ -34,6 +35,27 @@ int main(){
 	cout<<"Pos sign: "<<(obj_F.sign())<<endl;
 	cout<<"Zero no sign: "<<(obj_A.sign())<<endl;
 	//cout<<"Indeteminate for sign: "<<(obj_G.sign())<<endl;
+	// Test get_statistics and clear_statistics
+	stat st;
+	obj_C.get_statistics(st);
+	cout<<"get_statistics of obj_C: indet="<<(st.indeterminate_result_count)<<" arith="<<(st.arithmetic_op_count)<<endl;
+	obj_C.clear_statistics();
+	obj_C.get_statistics(st);
+	cout<<"clear_statistics of obj_C: indet="<<(st.indeterminate_result_count)<<" arith="<<(st.arithmetic_op_count)<<endl;
+	// Test Binary add, sub, and mult
+	itvf obj_H = obj_E + obj_F;
+	itvf obj_I = obj_E - obj_F;
+	itvf obj_J = obj_E * obj_F;
+	cout<<"[-7.339,-7.335] + [3.442,3.448]: "<<obj_H<<endl;
+	cout<<"[-7.339,-7.335] - [3.442,3.448]: "<<obj_I<<endl;
+	cout<<"[-7.339,-7.335] * [3.442,3.448]: "<<obj_J<<endl;
+	// Test less than
+	itvf obj_L(-7.342,-7.337);
+	cout<<"Must be true: "<<(obj_E < obj_F)<<endl;
+	cout<<"Must be false: "<<(obj_A < obj_E)<<endl;
+	//cout<<"Indeterminate: "<<(obj_L < obj_E)<<endl;
+
+
 	
 
 	return 0;
--- include/ra/interval.hpp
+++ include/ra/interval.hpp
@@ -3,8 +3,9 @@
 #include<cfenv>
 #include<stdexcept>
 #include<algorithm>
+#include<iostream>
 namespace ra::math {
-	#pragma STDC FENV_ACCESS ON
+
 	struct indeterminate_result : public std::runtime_error{
 		using std::runtime_error::runtime_error;
 	};
@@ -30,21 +31,31 @@ namespace ra::math {
 
 			struct statistics{
 				// The total number of indeterminate results encountered.
-				unsigned long indeterminate_result_count = 0;
+				unsigned long indeterminate_result_count;
 				// The total number of interval arithmetic operations.
-				unsigned long arithmetic_op_count = 0;
+				unsigned long arithmetic_op_count;
 			};
 
-			interval(real_type set_value = real_type(0)) : lower_(set_value), upper_(set_value) {}
-			interval(real_type lower, real_type upper) : lower_(lower), upper_(upper) {
-				if(lower > upper){ lower_=upper; upper_=lower; }
+			void keep_order() {
+				real_type temp;
+				if(lower_>upper_){
+					temp = lower_;
+					lower_ = upper_;
+					upper_ = temp;
+				}
 			}
+
+			interval(real_type set_value = real_type(0)) : lower_(set_value), upper_(set_value) {}
+			interval(real_type lower, real_type upper) : lower_(lower), upper_(upper) { keep_order(); }
 			real_type lower() const { return lower_; }
 			real_type upper() const { return upper_; }
+			
+			void incr_indeterminate_result_count() { ++(stat_.indeterminate_result_count); }
+			void incr_arithmetic_op_count() { ++(stat_.arithmetic_op_count); }
+
 			interval& operator+=(const interval& obj){
 				real_type upper_Temp(0);
 				real_type lower_Temp(0);
-				++(stat_.arithmetic_op_count);
 				rounding_mode_saver rms; // Save the rounding mode that should be restored
 				std::fesetround(FE_DOWNWARD);
 				lower_Temp = lower_ + obj.lower();
@@ -52,12 +63,12 @@ namespace ra::math {
 				upper_Temp = upper_ + obj.upper();
 				lower_ = lower_Temp;
 				upper_ = upper_Temp;
+				incr_arithmetic_op_count();
 				return *this;
 			}
 			interval& operator-=(const interval& obj){
 				real_type upper_Temp(0);
 				real_type lower_Temp(0);
-				++(stat_.arithmetic_op_count);
 				rounding_mode_saver rms; // Save the rounding mode that should be restored
 				std::fesetround(FE_DOWNWARD);
 				lower_Temp = lower_ - obj.upper();
@@ -65,12 +76,12 @@ namespace ra::math {
 				upper_Temp = upper_ - obj.lower();
 				lower_ = lower_Temp;
 				upper_ = upper_Temp;
+				incr_arithmetic_op_count();
 				return *this;
 			}
 			interval& operator*=(const interval& obj){
 				real_type upper_Temp(0);
 				real_type lower_Temp(0);
-				++(stat_.arithmetic_op_count);
 				rounding_mode_saver rms; // Save the rounding mode that should be restored
 				std::fesetround(FE_DOWNWARD);
 				lower_Temp = std::min(std::min((lower_*obj.lower()),(lower_*obj.upper())), std::min((upper_*obj.lower()), (upper_*obj.upper())));
@@ -78,31 +89,107 @@ namespace ra::math {
 				upper_Temp = std::max(std::max((lower_*obj.lower()),(lower_*obj.upper())), std::max((upper_*obj.lower()), (upper_*obj.upper())));
 				lower_ = lower_Temp;
 				upper_ = upper_Temp;
+				incr_arithmetic_op_count();
 				return *this;
 			}
 			
 			bool is_singleton() const { return (lower_==upper_)? true : false; }
 
 			int sign() const {
-				if((lower_ < real_type(0)) && (upper_ < real_type(0))){
-					return -1;
-				}
-				else if((lower_ > real_type(0)) && (upper_ > real_type(0))){
-					return 1;
-				}
-				else if((lower_ == real_type(0)) && (upper_ == real_type(0))){
-					return 0;
-				}
+				if((lower_ < real_type(0)) && (upper_ < real_type(0))){ return -1; }
+				else if((lower_ > real_type(0)) && (upper_ > real_type(0))){ return 1; }
+				else if((lower_ == real_type(0)) && (upper_ == real_type(0))){ return 0; }
 				else{
-					throw indeterminate_result("Indeterminate result");
+					throw indeterminate_result("Indeterminate sign of interval");
 				}
 			}
+			int sign() {
+				if((lower_ < real_type(0)) && (upper_ < real_type(0))){ return -1; }
+				else if((lower_ > real_type(0)) && (upper_ > real_type(0))){ return 1; }
+				else if((lower_ == real_type(0)) && (upper_ == real_type(0))){ return 0; }
+				else{
+					incr_indeterminate_result_count();
+					throw indeterminate_result("Indeterminate sign of interval");
+				}
+			}
+
+			void clear_statistics() {
+				stat_.indeterminate_result_count = 0;
+				stat_.arithmetic_op_count = 0;
+			}
+
+			void get_statistics(statistics& stat) {
+				stat = stat_;
+			}
+
 		private:
 			real_type upper_;
 			real_type lower_;
-			statistics stat_;
+			static statistics stat_;
 			
 	};
+	
+	template<class real_type>
+	typename interval<real_type>::statistics interval<real_type>::stat_ = {.indeterminate_result_count = 0, .arithmetic_op_count = 0};
+
+	template<class real_type>
+	interval<real_type> operator+(const interval<real_type>& obj_A, const interval<real_type>& obj_B){
+		real_type upper_Temp(0);
+		real_type lower_Temp(0);
+		rounding_mode_saver rms; // Save the rounding mode that should be restored
+		std::fesetround(FE_DOWNWARD);
+		lower_Temp = obj_A.lower() + obj_B.lower();
+		std::fesetround(FE_UPWARD);
+		upper_Temp = obj_A.upper() + obj_B.upper();
+		interval<real_type> result(lower_Temp,upper_Temp);
+		result.incr_arithmetic_op_count();
+		return result;
+	}
+
+	template<class real_type>
+	interval<real_type> operator-(const interval<real_type>& obj_A, const interval<real_type>& obj_B){
+		real_type upper_Temp(0);
+		real_type lower_Temp(0);
+		rounding_mode_saver rms; // Save the rounding mode that should be restored
+		std::fesetround(FE_DOWNWARD);
+		lower_Temp = obj_A.lower() - obj_B.upper();
+		std::fesetround(FE_UPWARD);
+		upper_Temp = obj_A.upper() - obj_B.lower();
+		interval<real_type> result(lower_Temp,upper_Temp);
+		result.incr_arithmetic_op_count();
+		return result;
+	}
+
+	template<class real_type>
+	interval<real_type> operator*(const interval<real_type>& obj_A, const interval<real_type>& obj_B){
+		real_type upper_Temp(0);
+		real_type lower_Temp(0);
+		rounding_mode_saver rms; // Save the rounding mode that should be restored
+		std::fesetround(FE_DOWNWARD);
+		lower_Temp = std::min(std::min((obj_A.lower()*obj_B.lower()),(obj_A.lower()*obj_B.upper())), std::min((obj_A.upper()*obj_B.lower()), (obj_A.upper()*obj_B.upper())));
+		std::fesetround(FE_UPWARD);
+		upper_Temp = std::max(std::max((obj_A.lower()*obj_B.lower()),(obj_A.lower()*obj_B.upper())), std::max((obj_A.upper()*obj_B.lower()), (obj_A.upper()*obj_B.upper())));
+		interval<real_type> result(lower_Temp,upper_Temp);
+		result.incr_arithmetic_op_count();
+		return result;
+	}
+
+	template<class real_type>
+	bool operator<(const interval<real_type>& obj_A, const interval<real_type>& obj_B){
+		if( (obj_A.upper()) < (obj_B.lower()) ) { return true; }
+		else if( (obj_A.lower()) >= (obj_B.upper()) ) { return false; }
+		else{
+			interval<real_type> temp;
+			temp.incr_indeterminate_result_count();
+			throw indeterminate_result("Indeterminate less than operator");
+		}
+	}
+
+	template<class real_type>
+	std::ostream& operator<<(std::ostream& outStream, const interval<real_type>& obj){
+		outStream<<"["<<(obj.lower())<<","<<(obj.upper())<<"]";
+		return outStream;
+	}
 
 
 }
