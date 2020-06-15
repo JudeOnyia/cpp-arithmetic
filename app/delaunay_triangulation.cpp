3813050045c41cffeade0ff47c11d88fc6816a55
Jude Onyia <judeonyia10@gmail.com>
2020-06-15 00:31:57 -0700

    1) applied the Lawson local optimization procedure

--- app/delaunay_triangulation.cpp
+++ app/delaunay_triangulation.cpp
@@ -8,21 +8,24 @@ using Kernel = CGAL::Cartesian<double>;
 using kernel = ra::geometry::Kernel<double>;
 using Triangulation = trilib::Triangulation_2<Kernel>;
 using hEit = Triangulation::Halfedge_iterator;
+using hE_hand = Triangulation::Halfedge_handle;
 using point = CGAL::Cartesian<double>::Point_2;
+using vector = CGAL::Cartesian<double>::Vector_2;
+
 /*
 using half_edge = Triangulation::Halfedge_handle;
 using vertex = Triangulation::Vertex_handle;
 */
 
-bool is_flippable(hEit edg_iter, kernel& obj){
-	if((*edg_iter).is_border_edge()){ return false; }
-	else{
+bool is_flippable(hE_hand edg_iter, kernel& obj){
+	//if((*edg_iter).is_border_edge()){ return false; }
+	//else{
 		point a = edg_iter->vertex()->point();
 		point b = edg_iter->next()->vertex()->point();
 		point c = edg_iter->opposite()->vertex()->point();
 		point d = edg_iter->opposite()->next()->vertex()->point();
 		return (obj.is_strictly_convex_quad(a,b,c,d));
-	}
+	//}
 }
 
 int main(int argc, char** argv){
@@ -30,14 +33,46 @@ int main(int argc, char** argv){
 	using std::endl;
 	Triangulation tri(std::cin);
 	kernel obj;
+	hE_hand tmp_hand;
+
+	
+	std::vector<hE_hand> suspect_list;
+	for (auto halfedgeIter = tri.halfedges_begin(); halfedgeIter !=tri.halfedges_end(); ++++halfedgeIter) {
+		tmp_hand = &*halfedgeIter;
+		if(is_flippable(tmp_hand,obj)){
+			suspect_list.push_back(tmp_hand);
+		}
+	}
+
+	while(!(suspect_list.empty())){
+		hE_hand  it = suspect_list.back();
+		if(is_flippable(it,obj)){
+			point c = it->vertex()->point();
+			point d = it->next()->vertex()->point();
+			point a = it->opposite()->vertex()->point();
+			point b = it->opposite()->next()->vertex()->point();
+			if(!(obj.is_locally_pd_delaunay_edge(a,b,c,d,vector(1,0),vector(1,1)))){
+				it = tri.flip_edge(it);
+				suspect_list.push_back(it->next());
+				suspect_list.push_back(it->next()->next());
+				suspect_list.push_back(it->opposite()->next());
+				suspect_list.push_back(it->opposite()->next()->next());
+			}
+		}
+		suspect_list.pop_back();
+	}
+
+	cout<<"Resulting Triangulation"<<endl;
+	tri.output_off(cout);
+
+	/*
 	int count = 0;
 	for (auto halfedgeIter = tri.halfedges_begin(); halfedgeIter !=tri.halfedges_end(); ++++halfedgeIter) {
 		if(is_flippable(halfedgeIter,obj)){ ++count; }
 		cout<<halfedgeIter->vertex()->point()<<endl;
 	}
 	cout<<"Count was: "<<count<<endl;
-
-	//std::vector<hEit> suspect_list;
+	*/
 
 	
 
